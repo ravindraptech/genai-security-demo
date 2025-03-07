4d74e3ca0d1671b25047a5029957f65a73e94732
ravindraptech <ravindrap.tech@gmail.com>
2025-03-07 22:56:14 +0530

    updated code with more features

--- local_llm_api_client.py
+++ local_llm_api_client.py
@@ -1,6 +1,19 @@
 import requests
+import argparse
+import json
 
 url = "http://localhost:5000/generate"
-payload = {"prompt": "Tell me an interesting fact about Snakes"}
+#payload = {"user_prompt": "Tell me an interesting fact about Pizza"}
+
+
+# Create CLI.
+parser = argparse.ArgumentParser()
+parser.add_argument("user_prompt", type=str, help="The user prompt.")
+args = parser.parse_args()
+user_prompt = args.user_prompt
+user_prompt = f"\"user_prompt\":\"{user_prompt}\""
+user_prompt_txt = '{' + user_prompt + '}'
+payload = json.loads(user_prompt_txt)
+
 response = requests.post(url, json=payload)
 print(response.json())
--- local_llm_api_server.py
+++ local_llm_api_server.py
@@ -9,12 +9,23 @@ generator = pipeline("text-generation", model="distilgpt2") # distilgpt2
 @app.route("/generate", methods=["POST"])
 def generate_text():
     data = request.get_json()
-    prompt = data.get("prompt", "")
-    if not prompt:
-        return jsonify({"error": "No prompt provided"}), 400
+    print(f"Received request: {data}")
+    user_prompt = data.get("user_prompt", "")
+    if not user_prompt:
+        return jsonify({"error": "No user_prompt provided"}), 400
+
+    # validate input
+    harmful_tokens = ['virus', 'hack', 'hate']
+    if any(x in user_prompt for x in harmful_tokens):
+        return jsonify({"error": "The model does not support generation of response which is harmful or insecure!"}), 403
+
+    # Generate text with context (RAG)
+    context = 'The name of the father of king Foobar was Daddybar. '
+    user_prompt = context +  user_prompt
 
     # Generate text
-    result = generator(prompt, max_length=50, num_return_sequences=1)
+    result = generator(user_prompt, num_return_sequences=1)
+    print(f"result: {result[0]["generated_text"]}")
     return jsonify({"response": result[0]["generated_text"]})
 
 if __name__ == "__main__":
